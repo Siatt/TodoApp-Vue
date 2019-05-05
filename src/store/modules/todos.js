0f1a9f2732da757643640cadbb14f48bf2bc4d10
Lucas <luclucsjoqvist@gmail.com>
2019-05-05 18:28:51 +0200

    add complete todo

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -30,13 +30,23 @@ const actions = {
         const limit = parseInt(e.target.options[e.target.options.selectedIndex].innerText)
         const res = await axios.get(`https://jsonplaceholder.typicode.com/todos?_limit=${limit}`)
         commit('setTodos', res.data)
+    },
+    async updateTodo({ commit }, updatedTodo) {
+        const res = await axios.put(`https://jsonplaceholder.typicode.com/todos/${updatedTodo.id}`, updatedTodo)
+        commit('updateTodo', res.data)
     }
 }
 // Mutate the state (update data,etc)
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
     newTodo: (state, todo) => state.todos.unshift(todo),
-    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
+    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id),
+    updateTodo: (state, updatedTodo) => {
+        const index = state.todos.findIndex(todo => todo.id === updatedTodo.id)
+        if (index !== -1) {
+            state.todos.splice(index, 1, updatedTodo)
+        }
+    }
 }
 
 export default {
