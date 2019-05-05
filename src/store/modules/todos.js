4063d95de0b2103f6e9ecf90cf0078b56f050522
Lucas <luclucsjoqvist@gmail.com>
2019-05-05 18:03:13 +0200

    add remove todo

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -20,12 +20,17 @@ const actions = {
         const res = await axios.post('https://jsonplaceholder.typicode.com/todos', { title, completed: false})
         commit('newTodo', res.data)
 
+    },
+    async deleteTodo ({ commit }, id) {
+        await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        commit('removeTodo', id)
     }
 }
 // Mutate the state (update data,etc)
 const mutations = {
     setTodos: (state, todos) => (state.todos = todos),
-    newTodo: (state, todo) => state.todos.unshift(todo)
+    newTodo: (state, todo) => state.todos.unshift(todo),
+    removeTodo: (state, id) => state.todos = state.todos.filter(todo => todo.id !== id)
 }
 
 export default {
