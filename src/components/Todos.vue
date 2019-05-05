b45e45c15edfb3188e52de2aafb2be4539c70777
Lucas <luclucsjoqvist@gmail.com>
2019-05-05 17:19:20 +0200

    add vuex state and todo module for gettes,actions,mutations, state

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="container">
         <h3>Todos</h3>
+        <div class="todos">
+            <div v-for="todo in getAllTodos" :key="todo.id" class="todo">
+                {{todo.title}}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-    name: "Todos"
+    name: "Todos",
+    computed: mapGetters(['getAllTodos'])
 }
 </script>
 
